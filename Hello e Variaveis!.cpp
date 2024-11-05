f5b74c6353d73d8c0aa19393f7dc7cabdb183f2f
Crospete <116527159+Crospete@users.noreply.github.com>
2024-11-05 16:18:38 -0300

    Add files via upload

---
+++ Hello e Variaveis!.cpp
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+AULA 05/11
+
+termos que se repetem podem ser escrito antes da execução como: using namespace std;
+<< - Operador de inserção em C++ (Deslocador de bits)
+
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World";
+    
+    
+    int x = 3;
+    int y = 2;
+    
+    cout << "\nx = " << x <<"\te y = " <<y;
+    
+    return 0;
+}
