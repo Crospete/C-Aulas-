253bf40e06db51f2d0006395b23686e61d6a990c
Crospete <116527159+Crospete@users.noreply.github.com>
2024-11-07 16:27:45 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/***Conceitos em C++
+ * 
+ * Todos objetos
+ * 
+ * Identidade - Propriedade que diferencia os objetos uns dos outros : Para C++ posição unida na memoria
+ * Comportamento - Define o que ele faz ou como reage perante mudanças : funcionalidades implementadas
+ * Estado - Define as propriedades do objeto, bem como seus valores : valores das variaveis
+ * 
+ * Classe - É um conjunto de objeto que compartilha caracteristicas em comum
+ * 
+ * Objeto - É uma instancia de uma classe
+ * 
+ * EX: 
+ * propriedades   |     Função
+ * ----------------------------
+ * Massa          |    Ligar()
+ * Velocidade     |    Desligar()
+ * Potencia       |    
+ * Nº de passageiros|    Ligar()
+ * 
+ * 
+ * 
+ * 
+
